cec5c7c649e48c13da7ff297bdf8028b0a2d64be
Mahmoud Ghanem <moghanem345@gmail.com>
2023-08-21 23:22:00 +0300

    use source instead of separting it, if we build basic CRUD api

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request) {
+
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required',
+            'price' => 'required',
+            'description' => 'required',
+        ]);
+        
+        return Product::create($request->all());
+    }
+
+    public function show($id)
+    {
+        return Product::find($id);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Product;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -15,18 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', function() {
-    return Product::all();
-});
+Route::source('products', ProductController::class);
 
-Route::post('/products', function() {
-    return Product::create([
-        'name' => 'Product One',
-        'slug' => 'product-one',
-        'description' => 'This is product one',
-        'price' => '99.99'
-    ]);
-});
+
+// Route::get('/products', [ProductController::class, 'index']);
+// Route::post('/products', [ProductController::class, 'store']);
+// Route::get('/products/{id}', [ProductController::class, 'show']); 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
