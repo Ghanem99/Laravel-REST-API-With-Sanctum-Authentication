e232016e93e7bc04428e00ed22adb3ecc8a032b5
Mahmoud Ghanem <moghanem345@gmail.com>
2023-08-22 06:11:13 +0300

    protect some routes and make the others public

--- routes/api.php
+++ routes/api.php
@@ -16,16 +16,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-route::resource('products', ProductController::class);
-// route::get('products/search/{name}', [ProductController::class, 'search']);
+// route::resource('products', ProductController::class);
+
+// Public Routes
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']); 
+route::get('products/search/{name}', [ProductController::class, 'search']);
 
 
-// Route::get('/products', [ProductController::class, 'index']);
-// Route::post('/products', [ProductController::class, 'store']);
-// Route::get('/products/{id}', [ProductController::class, 'show']); 
 
+// Protected Routes
 Route::group(['middleware'=>['auth:sanctum']], function () {
-    route::get('products/search/{name}', [ProductController::class, 'search']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    
 });
 
 
